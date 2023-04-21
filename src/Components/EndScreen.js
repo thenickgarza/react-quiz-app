6c6f2046ab733ea5b0f09769f168e2a4ac1f4e5b
Nick Garza <thenickgarza14@gmail.com>
2023-04-21 15:00:30 -0500

    finished end screen

--- src/Components/EndScreen.js
+++ src/Components/EndScreen.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, {useContext} from "react";
+import { QuizContext } from "../Helpers/Contexts";
+import { Questions } from "../Helpers/QuestionBank";
+import "../App.css"
 
 function EndScreen() {
-    return <div> End Screen</div>;
+    const { score, setScore, setGameState } = useContext(QuizContext)
+
+    const restartQuiz = () => {
+        setScore(0)
+        setGameState("menu")
+    }
+    return <div className="EndScreen">
+        <h1>Quiz Finsihed</h1>
+        <h3> {score} / {Questions.length}</h3>
+
+        <button onClick={restartQuiz}>Restart Quiz</button>
+    </div>;
 }
 
 export default EndScreen;
