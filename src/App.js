fd07ee9b95e273dc3514dffcb53fd3531c872084
Nick Garza <thenickgarza14@gmail.com>
2023-04-22 12:03:27 -0500

    switched the main h1 of the app to different text

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   
   return (
     <div className="App"> 
-    <h1>Quiz App</h1>
+    <h1>Get Ready!</h1>
     <QuizContext.Provider value={{gameState, setGameState, score, setScore}}>
     {gameState === 'menu' && <MainMenu />}
     {gameState === 'quiz' && <Quiz />}
